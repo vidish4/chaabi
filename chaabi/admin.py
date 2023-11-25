2351dff2bd08b1e2b59f6bcda2cfc2f9b9544a18
ComprehensiveDuck <84185837+vidish4@users.noreply.github.com>
2023-11-26 00:30:50 +0530

    Update admin.py

--- chaabi/admin.py
+++ chaabi/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Drinks
 from .models import Product
-from import_export.admin import ImportExportModelAdmin
-from .resources import ProductResource
+# from import_export.admin import ImportExportModelAdmin
+# from .resources import ProductResource
 
 admin.site.register(Drinks)
 admin.site.register(Product)
